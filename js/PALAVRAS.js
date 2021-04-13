389266892d1b3a7d7cf5e8a929ea5a6db9ac05d4
Débora Inocêncio <30512134+TrechosDeCodigos@users.noreply.github.com>
2021-04-12 22:24:46 -0300

    Add files via upload

---
+++ js/PALAVRAS.js
@@ -0,0 +1,130 @@
+{ 
+    type:" A ",
+  
+                {question:"A",options:['ABACAXI.jpg','IORGUTE.jpg','ELEFANTE.jpg','IGLU.jpg'],key:0},
+
+                {question:"A",options:['ARVORE.jpg','IOIO.jpg','ESQUILO.jpg' ,'ILHA.jpg'],key:0},
+
+                {question:"A",options:['ESPADA.jpg','OLHO.jpg','IORGUTE.jpg','ABACATE.jpg'],key:3},
+
+                {question:"A",options:['OURO.jpg' ,'ABOBORA.jpg','ILHA.jpg','URUBU.jpg'],key:1},
+                
+                {question:"A",options:['ARANHA.jpg','ILHA.jpg','URUBU.jpg','OVO.jpg'],key:0},
+
+                {question:"A",options:['ESCOVA.jpg','OVELHA.jpg','UVA.jpg','ARARA.jpg'],key:3},
+                
+                {question:"A",options:['ONIBUS.jpg','AVIAO.jpg','UNHAS.jpg','ELEFANTE.jpg'],key:1},
+
+                {question:"A",options:['ELEFANTE.jpg','OVO.jpg','ASTRONAUTA.jpg','UVA.jpg'],key:2},
+
+                {question:"A",options:['IMA.jpg','IORGUTE.jpg','ARMARIO.jpg','ELEFANTE.jpg'],key:2},
+
+                {question:"A",options:['ABELHA.jpg','ESCADA.jpg','ESPANTALHO.jpg','ESPADA.jpg'],key:0}
+
+               ]
+   },
+
+   { 
+        type:" E ",
+        
+                   {question:"E",options:['ABACATE.jpg','ESPANTALHO.jpg','OVELHA.jpg','IGLU.jpg'],key:1},
+
+                   {question:"E",options:['OLHO.jpg','IORGUTE.jpg','ELEFANTE.jpg','ABACAXI.jpg'],key:2},
+
+                   {question:"E",options:['ABOBORA.jpg','ESCOVA.jpg','OLEO.jpg' ,'OVO.jpg',],key:1},
+
+                   {question:"E ",options:['ESQUILO.jpg','ABOBORA.jpg','ILHA.jpg','OLEO.jpg'],key:0},
+
+                   {question:"E",options:['OURO.jpg','URSO.jpg','ESTRADA.jpg','IMA.jpg',],key:2},
+
+                   {question:"E",options:['ESTRELA.jpg','ARANHA.jpg','ILHA.jpg','ABOBORA.jpg'],key:0},
+
+                   {question:"E",options:['ABACATE.jpg','ABACAXI.jpg','OURO.jpg','ESPADA.jpg'],key:3},
+
+                   {question:"E",options:['OLHO.jpg','ESCADA.jpg','ABACAXI.jpg','AVIAO.jpg'],key:1},
+
+                   {question:"E ",options:['ILHA.jpg','UNICORNIO.jpg','ESPAGUETE.jpg','IOIO.jpg'],key:2},
+
+                   {question:"E",options:['ESCORREGADOR.jpg','UVA.jpg','OVO.jpg','ARVORE.jpg'],key:0}
+
+               ]
+   },
+
+    {
+        type:" I ",
+        
+                  {question:"I",options:['ARARA.jpg','ESTRADA.jpg','IORGUTE.jpg', 'OLEO.jpg'],key:2},
+
+                  {question:"I",options:['INCENDIO.jpg','ALHO.jpg','ESTRADA.jpg','AVIAO.jpg'],key:0},
+
+                  {question:"I",options:['OVO.jpg' ,'ESCOVA.jpg','ILHA.jpg','ALHO.jpg'],key:2},
+
+                  {question:"I",options:['ABOBORA.jpg','IMA.jpg','ESCADA.jpg', 'OURO.jpg'],key:1},
+
+                  {question:"I",options:['IGLU.jpg','UNIVERSO.jpg','ABACAXI.jpg','OLHO.jpg'],key:0},
+
+                  {question:"I",options:['OLHO.jpg','ARARA.jpg','ABACATE.jpg','INJECAO.jpg'],key:3},
+
+                  {question:"I",options:['ABACATE.jpg','IOIO.jpg','OLHO.jpg','ELEFANTE.jpg'],key:1},
+                  
+                  {question:"I",options:['IGREJA.jpg','URUBU.jpg','OCULOS.jpg','ESCORREGADOR.jpg'],key:0},
+
+                  {question:"I",options:['AVIAO.jpg','UM.jpg','ONIBUS.jpg','INSETO.jpg'],key:3},
+
+                  {question:"I",options:['ELEFANTE.jpg','OVELHA.jpg','ISQUEIRO.jpg','ABELHA.jpg'],key:2}
+
+
+        ]
+
+     },
+
+     
+
+   {
+        type:" O ",
+        
+                     {question:"O",options:['ABACAXI.jpg','URUBU.jpg','ESPAGUETE.jpg','ONDA.jpg'],key:3},
+
+                     {question:"O",options: ['OITO.jpg','AVIAO.jpg','UNHAS.jpg','IMA.jpg'],key:0},
+
+                     {question:"O",options:['ARARA.jpg','ELEFANTE.jpg','ORELHA.jpg','ESTRELA.jpg'],key:2},
+
+                     {question:"O",options:['IORGUTE.jpg','ABACAXI.jpg','OLHO.jpg','UNIVERSO.jpg'],key:2},
+
+                     {question:"O",options:['UVA.jpg','OSSO.jpg','ABOBORA.jpg' ,'ESTRELA.jpg'],key:1},
+
+                     {question:"O",options:['OCULOS.jpg','AVIAO.jpg','URUBU.jpg', 'ESCOVA.jpg'],key:0},
+
+                     {question:"O",options:['IMA.jpg','ESPAGUETE.jpg','ARARA.jpg','ONIBUS.jpg'],key:3},
+
+                     {question:"O",options:['AVIAO.jpg','ELEFANTE.jpg','ONCA.jpg','ARARA.jpg'],key:2},
+
+                     {question:"O",options:['UNICORNIO.jpg','ABELHA.jpg','OURO.jpg','UM.jpg'],key:2},
+
+                     {question:"O",options:['OVELHA.jpg','ELEFANTE.jpg','ABELHA.jpg','IGLU.jpg'],key:0}    
+
+                   ]         
+               
+    },
+
+
+    {
+        type:" U ",
+      
+                   {question:"U",options:['ABACATE.jpg','ABACAXI.jpg','UNIVERSO.jpg','ARARA.jpg'],key:2},
+
+                   {question:"U",options:['ARARA.jpg','UNIFORME.jpg','ABACAXI.jpg','OURO.jpg'],key:1},
+
+                   {question:"U",options:['OURO.jpg','ALHO.jpg','ESTRELA.jpg','UNHAS.jpg' ],key:3},
+
+                   {question:"U",options:['ABOBORA.jpg','ABACAXI.jpg','URSO.jpg','ELEFANTE.jpg'],key:2},
+
+                   {question:"U",options:['ILHA.jpg' ,'ESCOVA.jpg','ASTRONAUTA.jpg', 'URUBU.jpg'],key:3},
+
+                   {question:"U",options:['IGLU.jpg','UVA.jpg','IORGUTE.jpg','IOIO.jpg'],key:1},
+
+                   {question:"U",options:['UM.jpg','ASTRONAUTA.jpg','ILHA.jpg','IGLU.jpg'],key:0},
+
+                   {question:"U",options:['OVO.jpg','IMA.jpg','UNICORNIO.jpg','ESTRELA.jpg'],key:2}
+
+                   
---
+++ js/script.js
@@ -0,0 +1,7 @@
+function Question(text, choices, answer){
+     this.text=text;
+     this.choices = choices;
+     this.answer = answer; 
+
+}
+// Parei em 18:56
